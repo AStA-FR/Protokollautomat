8d7b4b2117085da34b0a8f9e83f9ed7db87a5174
hanserj <hanser.jona@gmail.com>
2020-05-26 19:51:14 +0200

    Add some feedback for the user.

--- AStA_Protokoll_Gen.sh
+++ AStA_Protokoll_Gen.sh
@@ -24,11 +24,13 @@ function replace_results {
     # This replaces the results of the polls
     # Please be aware that they need to be of the following format:
     # (j/n/e) (1/2/3) => Some text that describes the result.
+    printf "Die Ergebnisse der Abstimmung werden hübsch gemacht.\n"
     sed -i "s,(j/n/e) (\([0-9]*\)/\([0-9]*\)/\([0-9]*\)) => \(.*\),| j | n | e | Ergebnis: |\n| \1 | \2 | \3 | \4 |\n," "$INPUTFILE"
 }
 
 function replace_absences {
     # Streamlining.
+    printf "(Un)entschuldigte Abwesenheiten werden eingetragen.\n"
     # They just weren't there.
     sed -i "s,- n/a,- Nicht anwesend." "$INPUTFILE"
     # They provided a valid reason for not being there.
@@ -37,6 +39,7 @@ function replace_absences {
 
 function generate_output {
     # Generate the pdf-output with pandoc.
+    printf "Das Protokoll wird erstellt...\n"
     pandoc -s $INPUTFILE -o $BNAME.pdf
 }
 
@@ -54,5 +57,4 @@ replace_absences
 replace_results
 generate_output
 
-
-printf 'Bye!\n'
+printf 'Fertig.\nTschüssle!\n'

59f463732fdc3dbeeb962973046c684a21c4a445
hanserj <hanser.jona@gmail.com>
2020-05-27 16:59:52 +0200

    Add a funktion that removes broken quotation marks; fixes minor bug in sed-scripts

--- AStA_Protokoll_Gen.sh
+++ AStA_Protokoll_Gen.sh
@@ -32,9 +32,15 @@ function replace_absences {
     # Streamlining.
     printf "(Un)entschuldigte Abwesenheiten werden eingetragen.\n"
     # They just weren't there.
-    sed -i "s,- n/a,- Nicht anwesend." "$INPUTFILE"
+    sed -i "s,- n/a,- Nicht anwesend.," "$INPUTFILE"
     # They provided a valid reason for not being there.
-    sed -i "s,- ent,- Entschuldigt." "$INPUTFILE"
+    sed -i "s,- ent,- Entschuldigt.," "$INPUTFILE"
+}
+
+function replace_broken_stuff {
+    # Helluvalot is fecked.
+    # Quotation marks
+    sed -i "s,\",'',"
 }
 
 function generate_output {
@@ -53,6 +59,7 @@ function cleanup {
 # The main!
 # Here happens everything!
 init $1
+replace_broken_stuff
 replace_absences
 replace_results
 generate_output

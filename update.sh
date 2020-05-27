fa966cddbcc2654fde9b50a49bf4757858e898be
hanserj <hanser.jona@gmail.com>
2020-05-27 18:21:52 +0200

    Add the updater

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Copyright 2020 Jonathan Hanser <hanserj@tf.uni-freiburg.de>
+
+# To make life easier for the users, hide the process of updating the tool.
+
+git pull
+cp ./AStA_Protokoll_Gen.sh ../
